97c271a9044ae6a1d29618c50619e970a1b17dee
miyako <miyako@arch.(none)>
2010-02-12 23:58:16 -0600

    better keypress testing

--- test.c
+++ test.c
@@ -2,23 +2,61 @@
 #include <stdio.h>
 #include <string.h>
 
-int read_loop(usblcd_operations* screen)
+int get_key_updown(usblcd_operations* screen)
 {
+    int val;
     usblcd_event* e;
-    while(e = screen->read_events(screen))
+    do
     {
-        if(e->type) //not keyboard
-            continue;
-        printf("got %d bytes\n",e->length);
-        int dval = (int)(e->data);
-        printf("byte one: %d\n",(int)(e->data[0]));
-        printf("byte two: %d\n",(int)(e->data[1]));
-        if(dval)
-            printf("read key:%d\n",dval);
-        else
-            printf("key released\n");
+        e = screen->read_events(screen);
+    }while(e->type);
+    return (int)(e->data[0]);
+}
+
+int keypoll(usblcd_operations* screen)
+{
+    int key_data = get_key_updown(screen);
+    if(0 != get_key_updown(screen))
+    {
+        printf("Error, expected key to be released\n");
+        return -1;
     }
-    return 0;
+    return key_data;
+}
+
+const char* key_tostr(int key)
+{
+    switch(key)
+    {
+        case 0x0:
+            return "KEY_RELEASED"; 
+        case 0x1:
+            return "KEY_PLUS"; 
+        case 0x2:
+            return "KEY_MINUS"; 
+        case 0x3:
+            return "KEY_F1"; 
+        case 0x4:
+            return "KEY_F2"; 
+        case 0x5:
+            return "KEY_F3"; 
+        case 0x6:
+            return "KEY_F4"; 
+        case 0x7:
+            return "KEY_F5"; 
+        case 0x8:
+            return "KEY_LEFT"; 
+        case 0x9:
+            return "KEY_RIGHT"; 
+        case 0xa:
+            return "KEY_UP"; 
+        case 0xb:
+            return "KEY_DOWN"; 
+        case 0xc:
+            return "KEY_ENTER"; 
+        default:
+            return "KEY_UNKNOWN";
+    }
 }
 
 int main(void)
@@ -29,8 +67,8 @@ int main(void)
     /* init the USB LCD */
     mylcd->init(mylcd);
     /* sets backlight to on */
-    mylcd->backlight(mylcd,0);
-    read_loop(mylcd);
+    mylcd->backlight(mylcd,1);
+    printf("%s\n",key_tostr(keypoll(mylcd)));
     /* close the USB LCD device */
     mylcd->close(mylcd);
     return 0;
